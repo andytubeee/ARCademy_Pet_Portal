0e8e77094745bfecc6fdbb365d9db134373ce171
andytubeee <andy.tubeee@gmail.com>
2020-06-06 19:35:40 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity/flutter_unity.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your Pet',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Your Pet'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ARCademy'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top:300),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => UnityViewPage()));
+                },
+                child: Text('Your Pet'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.account_balance_wallet),
+                  SizedBox(width: 10,),
+                  Text("1500 XP")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UnityViewPage extends StatefulWidget {
+  @override
+  _UnityViewPageState createState() => _UnityViewPageState();
+}
+
+class _UnityViewPageState extends State<UnityViewPage> {
+  UnityViewController unityViewController;
+
+  double _sliderValue = 1.0;
+  String _projectKey = 'wispy-boat-5249';
+  String _entryID = '40d7381d-5789-43ae-9f14-8c3d0036ac1a';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Pet'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          UnityView(
+            onCreated: onUnityViewCreated,
+            onReattached: onUnityViewReattached,
+            onMessage: onUnityViewMessage,
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: Card(
+              elevation: 10,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Text("Pet Size:"),
+                  ),
+                  Slider(
+                    onChanged: (value) {
+                      setState(() {
+                        _sliderValue = value;
+                      });
+                      setScale(value.toString());
+                    },
+                    value: _sliderValue,
+                    min: 0.1,
+                    max: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onUnityViewCreated(UnityViewController controller) {
+    print('onUnityViewCreated');
+
+    unityViewController = controller;
+  }
+
+  void onUnityViewReattached(UnityViewController controller) {
+    print('onUnityViewReattached');
+  }
+
+  void onUnityViewMessage(UnityViewController controller, String message) {
+    print('onUnityViewMessage');
+
+    print(message);
+  }
+
+  void setScale(String scale) {
+    String url = 'https://console.echoar.xyz/post';
+    print(scale);
+    http.post(url, body: {
+      'key': _projectKey,
+      'entry': _entryID,
+      'data': 'scale',
+      'value': scale
+    }).then((value) => print(value.body));
+  }
+}
